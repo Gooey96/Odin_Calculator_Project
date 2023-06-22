05a237454a4ca9d74b956abd34bf492740815720
Luqman <bluebearymuffin55@gmail.com>
2023-06-22 16:08:44 +0800

    Change from console log to return

--- scripts.js
+++ scripts.js
@@ -3,19 +3,19 @@ let secondNumbers = '';
 let operator = '';
 
 function add(a, b) {
-  console.log(a + b);
+  return a + b;
 };
 
 function subtract(a, b) {
-  console.log(a - b);
+  return a - b;
 };
 
 function multiply(a, b) {
-  console.log(a * b);
+  return a * b;
 };
 
 function divide(a, b) {
-  console.log(a / b);
+  return a / b;
 };
 
 function operate(a, operation, b) {
