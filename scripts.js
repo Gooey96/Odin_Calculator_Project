12d4b30c01aa24666fa3f724a4fa88608e465cd5
Luqman <bluebearymuffin55@gmail.com>
2023-06-26 12:26:10 +0800

    Delete the else if code

--- scripts.js
+++ scripts.js
@@ -78,9 +78,6 @@ operatorButtons.forEach((button) => {
       firstDisplay.textContent = `${firstNumbers} ${operator}`;
       console.log("It's Works");
     }
-    else if(firstDisplay.textContent === '0') {
-      firstDisplay.textContent = `${firstNumbers} ${e.target.textContent}`
-      console.log("It's does not equal to the first number");
-    }
+    
   })
 })
